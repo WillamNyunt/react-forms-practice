e56a43250db793f25a1c1578c175d644581c0e76
WillamNyunt <williamnyunt@outlook.com>
2024-04-08 12:14:34 +0100

    Linking email & password t ostate

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,44 @@
+import { useState } from "react";
+
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log('submitted')
+  }
+
+  function handleUserInput(identifier, value) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+    }))
+  }
+
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => handleUserInput('email', event.target.value)} value={enteredValues.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => handleUserInput('password', event.target.value)} value={enteredValues.password} />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" >Login</button>
       </p>
     </form>
   );
